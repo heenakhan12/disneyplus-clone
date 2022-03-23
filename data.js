7b160d83a85f668ee073215a854086b581dd4d28
Heena Kouser <heenakhan786333@gmail.com>
2022-03-23 18:45:48 +0530

    first commit

---
+++ data.js
@@ -0,0 +1,28 @@
+let movies= [
+    {
+        name:'falcon and the winter soldier',
+        des:'this is the movie of the year, this is the oscar movie.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.',
+        image:'./images/slider 2.png'
+    },
+    {
+        name:'Loki',
+        des:'this is the movie of the year, this is the oscar movie.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.',
+        image:'./images/slider 1.png'
+    },
+    {
+        name:'Wanda Vision',
+        des:'this is the movie of the year, this is the oscar movie.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.',
+        image:'./images/slider 3.png'
+    },
+    {
+        name:'raya and the last dragon',
+        des:'this is the movie of the year, this is the oscar movie.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.',
+        image:'./images/slider 4.png'
+    },
+    {
+        name:'Luca',
+        des:'this is the movie of the year, this is the oscar movie.Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna.',
+        image:'./images/slider 5.png'
+    }
+
+]
